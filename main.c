f46d40b1426c9d1fe2ad39efabecda5ba40e479e
odalson <melvin4gups@gmail.com>
2024-01-21 19:41:12 -0800

    updated monty

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0);
+/** 
+ * main - monty code interpreter
+ * @argc: number of argument
+ * return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: cant open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(conent, &stack, counter, file);
+		}
+		freee(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
+}
---
+++ nop.c
@@ -0,0 +1,13 @@
+#Include "monty.h"
+/**
+ * f_nop - nothing
+ * @head: stack head
+ * @counter: line number
+ * return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
+
